2ed70f3f614221e84251217c506b90001cac611f
StefanoMela <stefano.mela25@gmail.com>
2026-02-14 11:46:46 +0100

    ESLint & Prettier set up

---
+++ eslint.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import withNuxt from './.nuxt/eslint.config.mjs'
+import prettier from 'eslint-plugin-prettier'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default withNuxt(
+  eslintConfigPrettier,
+  {
+    plugins: { prettier },
+    rules: {
+      'prettier/prettier': 'error'
+    }
+  }
+)
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/fonts', '@nuxtjs/tailwindcss'],
+  modules: ['@nuxt/fonts', '@nuxtjs/tailwindcss', '@nuxt/eslint'],
   fonts: {
     defaults: {
       weights: [400, 500, 600, 700],
